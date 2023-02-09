123d9181a1490e4c915a53e056ec90fcbbc01717
nikitaantsev <71016018+nikitaantsev@users.noreply.github.com>
2023-02-09 23:01:21 +0300

    Add files via upload

---
+++ lab.1/lab.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab._1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Func<Action<int>, bool, char, string> lambda = (x, flag, y) =>
+            {
+                x(100);
+                return (flag) ? "flag is true " + y : "flag is false " + y;
+            };
+            Action<int> TT = MyFunc;
+            Console.WriteLine(lambda(TT, true, 'Y'));
+            Console.WriteLine(lambda(TT, false, 'N'));
+            Console.ReadLine();
+        }
+        static void MyFunc(int s)
+        {
+            s++;
+            Console.WriteLine(s);
+        }
+    }
+}
